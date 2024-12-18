8cc55fac85989ee38bfee5b81803eaed2299575f
Dmytro Koliesnichenko <koliesnichenko2018@ukr.net>
2024-12-18 16:41:26 +0200

    initial setup, change settings.py, create models in models.py

--- agency/models.py
+++ agency/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ("name", )
+
+    def __str__(self):
+        return self.name
+
+
+class Redactor(AbstractUser):
+    year_of_experience = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = "redactor"
+        verbose_name_plural = "redactors"
+
+    def __str__(self):
+        return f"{self.username}: ({self.first_name} {self.last_name})"
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now=True)
+    topic = models.ForeignKey(
+        Topic,
+        on_delete=models.CASCADE,
+        related_name="newspapers"
+    )
+    publishers = models.ManyToManyField(
+        Redactor,
+        related_name="newspapers"
+    )
+
+    class Meta:
+        ordering = ("title", )
+
+    def __str__(self):
+        return f"{self.topic}, {self.title}: {self.content} ({self.published_date})"
