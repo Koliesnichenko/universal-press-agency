a084c3ce0d478c44a7d50eae1ec9a5d40e909b9a
Dmytro Koliesnichenko <koliesnichenko2018@ukr.net>
2024-12-23 14:06:07 +0200

    created index in views.py

--- agency/views.py
+++ agency/views.py
@@ -1,4 +1,22 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from agency.models import Newspaper, Redactor, Topic
+
+
 def index(request: HttpRequest) -> HttpResponse:
+    num_newspapers = Newspaper.objects.count()
+    num_redactors = Redactor.objects.count()
+    num_topics = Topic.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_newspapers": num_newspapers,
+        "num_redactors": num_redactors,
+        "num_topics": num_topics,
+        "num_visits": num_visits + 1,
+    }
+    return render(request, "agency/index.html", context=context)
+
