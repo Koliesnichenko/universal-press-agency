0fe18112184d5794a608393680f6ea6bcc8f34b0
Dmytro Koliesnichenko <koliesnichenko2018@ukr.net>
2024-12-23 17:03:38 +0200

    created ListView for Topic, Newspaper, Redactor

--- agency/views.py
+++ agency/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+
+from django.views.generic import ListView
 
 from agency.models import Newspaper, Redactor, Topic
 
@@ -20,3 +23,22 @@ def index(request: HttpRequest) -> HttpResponse:
     }
     return render(request, "agency/index.html", context=context)
 
+
+class TopicListView(LoginRequiredMixin, ListView):
+    model = Topic
+    context_object_name = "topic_list"
+    template_name = "agency/topic_list.html"
+    paginate_by = 5
+
+
+class NewspaperListView(LoginRequiredMixin, ListView):
+    model = Newspaper
+    context_object_name = "newspaper_list"
+    template_name = "agency/newspaper_list.html"
+    paginate_by = 5
+    queryset = Newspaper.objects.select_related("topic")
+
+
+class RedactorListView(LoginRequiredMixin, ListView):
+    model = Redactor
+    paginate_by = 5
