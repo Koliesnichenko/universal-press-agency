5e4b7a52543e6044a7896efe64575400dedceea8
Dmytro Koliesnichenko <koliesnichenko2018@ukr.net>
2024-12-24 12:49:28 +0200

    created urls for redactors, newspapers, topics ListViews

--- agency/urls.py
+++ agency/urls.py
@@ -1,10 +1,28 @@
 from django.urls import path
 
 from . import views
-
+from .views import (
+    TopicListView,
+    NewspaperListView, RedactorListView,
+)
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path(
+        "topics/",
+        TopicListView.as_view(),
+        name="topic-list",
+    ),
+    path(
+        "newpapers/",
+        NewspaperListView.as_view(),
+        name="newspaper-list",
+    ),
+    path(
+        "redactors/",
+        RedactorListView.as_view(),
+        name="redactor-list",
+    ),
 ]
 
 app_name = "agency"
