89ffed8e21f2c4dd04fd58c299e44ae7b459d061
Dmytro Koliesnichenko <koliesnichenko2018@ukr.net>
2025-01-02 16:51:47 +0200

    Add create, update, delete, detail urls for Redactors, Newspapers, Topics

--- agency/urls.py
+++ agency/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
 
-from . import views
 from .views import (
     index,
     TopicListView,
+    TopicCreateView,
+    TopicUpdateView,
+    TopicDeleteView,
     NewspaperListView,
+    NewspaperCreateView,
+    NewspaperUpdateView,
+    NewspaperDeleteView,
     RedactorListView,
-    RedactorDetailView, RedactorCreateView,
+    RedactorDetailView,
+    RedactorCreateView,
+    RedactorYearOfExperienceUpdateView,
+    RedactorDeleteView,
 )
 
 urlpatterns = [
@@ -17,9 +25,39 @@ urlpatterns = [
         name="topic-list",
     ),
     path(
+        "topics/create/",
+        TopicCreateView.as_view(),
+        name="topic-create",
+    ),
+    path(
+        "topics/<int:pk>/update/",
+        TopicUpdateView.as_view(),
+        name="topic-update",
+    ),
+    path(
+        "topics/<int:pk>/delete/",
+        TopicDeleteView.as_view(),
+        name="topic-delete",
+    ),
+    path(
         "newspapers/",
         NewspaperListView.as_view(),
         name="newspaper-list",
+    ),
+    path(
+        "newspapers/create/",
+        NewspaperCreateView.as_view(),
+        name="newspaper-create",
+    ),
+    path(
+        "newspapers/<int:pk>/update/",
+        NewspaperUpdateView.as_view(),
+        name="newspaper-update",
+    ),
+    path(
+        "newspapers/<int:pk>/delete/",
+        NewspaperDeleteView.as_view(),
+        name="newspaper-delete",
     ),
     path(
         "redactors/",
@@ -27,14 +65,24 @@ urlpatterns = [
         name="redactor-list",
     ),
     path(
-        "redactor/<int:pk>/",
+        "redactors/create/",
+        RedactorCreateView.as_view(),
+        name="redactor-create",
+    ),
+    path(
+        "redactors/<int:pk>/",
         RedactorDetailView.as_view(),
         name="redactor-detail",
     ),
     path(
-        "redacor/create/",
-        RedactorCreateView.as_view(),
-        name="redactor-create",
+        "redactors/<int:pk>/update/",
+        RedactorYearOfExperienceUpdateView.as_view(),
+        name="redactor-update",
+    ),
+    path(
+        "redactors/<int:pk>/delete/",
+        RedactorDeleteView.as_view(),
+        name="redactor-delete",
     )
 ]
 
