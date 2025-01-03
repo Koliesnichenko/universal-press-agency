01ccb0dc85513fb8b13fc30304694f08436d4e31
Dmytro Koliesnichenko <koliesnichenko2018@ukr.net>
2025-01-03 13:06:34 +0200

    add create/update forms, search forms, years of experience validator

--- agency/forms.py
+++ agency/forms.py
@@ -1,6 +1,36 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.core.exceptions import ValidationError
 
-from agency.models import Redactor
+from agency.models import Redactor, Topic, Newspaper
+
+
+class TopicCreationForm(forms.ModelForm):
+    class Meta:
+        model = Topic
+        fields = ["name"]
+        widgets = {
+            "name": forms.TextInput(
+                attrs={
+                    "class": "form-control", "placeholder": "Enter topic name"
+                }
+            ),
+        }
+
+
+class TopicSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Search by topic"
+            }
+        )
+    )
 
 
 class RedactorCreationForm(UserCreationForm):
@@ -11,3 +41,81 @@ class RedactorCreationForm(UserCreationForm):
             "last_name",
             "years_of_experience",
         )
+
+
+class RedactorSearchForm(forms.ModelForm):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Search by username"
+            }
+        )
+    )
+
+
+class BaseNewspaperForm(forms.ModelForm):
+    publishers = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple(),
+        required=False,
+    )
+
+    class Meta:
+        model = Newspaper
+        fields = (
+            "title",
+            "content",
+            "topic",
+            "publishers",
+        )
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control"}),
+            "content": forms.Textarea(attrs={"class": "form-control"}),
+            "topic": forms.Select(attrs={"class": "form-control"}),
+        }
+
+
+class NewspaperUpdateForm(BaseNewspaperForm):
+    pass
+
+
+class NewspaperCreationForm(BaseNewspaperForm):
+    pass
+
+
+class NewspaperSearchForm(forms.ModelForm):
+    title = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Search by title"
+            }
+        )
+    )
+
+
+class RedactorYearsOfExperienceCreationForm(forms.ModelForm):
+    class Meta:
+        model = Redactor
+        fields = ["years_of_experience"]
+
+    def clean_years_of_experience(self):
+        return validate_years_of_experience(self.cleaned_data["years_of_experience"])
+
+
+def validate_years_of_experience(
+        years_of_experience,
+):
+    if years_of_experience < 0:
+        raise ValidationError("Years of experience should be more than 0")
+    if years_of_experience > 100:
+        raise ValidationError("Years of experience should be less than 100")
+
+    return years_of_experience
