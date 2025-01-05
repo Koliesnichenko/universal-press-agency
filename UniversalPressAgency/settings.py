f252d018036509e712424e66806bb74fad5807a3
Dmytro Koliesnichenko <koliesnichenko2018@ukr.net>
2025-01-06 00:20:37 +0200

    Add Debug toolbar

--- UniversalPressAgency/settings.py
+++ UniversalPressAgency/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
 
 # Application definition
 
@@ -40,10 +44,12 @@ INSTALLED_APPS = [
     "agency",
     "crispy_forms",
     "crispy_bootstrap4",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
--- UniversalPressAgency/urls.py
+++ UniversalPressAgency/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("agency.urls", namespace="agency")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
