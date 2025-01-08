72feb1af128525a6d3e59adff3bb3dff858ad16e
Dmytro Koliesnichenko <koliesnichenko2018@ukr.net>
2025-01-08 18:10:00 +0200

    add email field for redactor creation form & redactor_list.html

--- agency/forms.py
+++ agency/forms.py
@@ -34,11 +34,12 @@ class TopicSearchForm(forms.Form):
 
 
 class RedactorCreationForm(UserCreationForm):
-    class Meta:
+    class Meta(UserCreationForm.Meta):
         model = Redactor
         fields = UserCreationForm.Meta.fields + (
             "first_name",
             "last_name",
+            "email",
             "years_of_experience",
         )
 
