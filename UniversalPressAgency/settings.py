883f44343cfc14350a5bd4579638cbd6415e27b4
Dmytro Koliesnichenko <koliesnichenko2018@ukr.net>
2025-01-15 22:20:40 +0200

    Add assets settings to project
    Configured static and media settings in settings.py and added assert in context_processors.py

--- UniversalPressAgency/settings.py
+++ UniversalPressAgency/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "agency.context_processors.cfg_assets_root",
             ],
         },
     },
@@ -130,6 +131,7 @@ STATICFILES_DIRS = [
  BASE_DIR / "static",
 ]
 
+ASSETS_ROOT = "/static/assets"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
